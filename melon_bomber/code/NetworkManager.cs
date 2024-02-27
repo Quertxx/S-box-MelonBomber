1eeee2116d0299556c01fa07296f57967807b344
Jack Burrup <62547604+Quertxx@users.noreply.github.com>
2024-02-27 03:19:56 +0000

    wasd

--- melon_bomber/code/NetworkManager.cs
+++ melon_bomber/code/NetworkManager.cs
@@ -1,7 +1,22 @@
 using Sandbox;
+using Sandbox.Network;
 
 public sealed class NetworkManager : Component
 {
+	[System.Obsolete]
+	protected override async void OnStart()
+	{
+		base.OnStart();
+		var list = await GameNetworkSystem.QueryLobbies();
+		if (list.ToArray().Length>0)
+		{
+			GameNetworkSystem.Connect(list.First().LobbyId);
+		}
+		else
+		{
+			GameNetworkSystem.CreateLobby();
+		}
+	}
 	protected override void OnUpdate()
 	{
 
